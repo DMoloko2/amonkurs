3ac04520d1c8763bf3ae2ee3d9f9b019cfbffae4
IgorSuslov254 <igirsuslov@gmail.com>
2019-11-28 12:33:34 +0300

    Исходники

---
+++ application/views/main_view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+		<link rel="stylesheet" 		href="<?= base_url(); ?>MDB/css/my.css">
+		<link rel="stylesheet" 		href="<?= base_url(); ?>MDB/css/bootstrap.min.css">
+		<link rel="stylesheet"	 	href="<?= base_url(); ?>MDB/css/mdb.min.css">
+		<link rel="stylesheet" 		href="<?= base_url(); ?>MDB/css/style.css">
+		<link rel="stylesheet" 		href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+		<title>РЖД</title>
+	</head>
+	<body>
+    <main>
+      <div class="container">
+        <h1 class="text-center">Проверка</h1>
+      </div>
+    </main>
+  </body>
+    <script src="<?= base_url(); ?>MDB/js/jquery-3.4.1.min.js"></script>
+    <script src="<?= base_url(); ?>MDB/js/bootstrap.min.js"></script>
+    <script src="<?= base_url(); ?>MDB/js/mdb.min.js"></script>
+    <script src="<?= base_url(); ?>MDB/js/script.js"></script>
+    <script type="text/javascript" src="<?= base_url(); ?>MDB/js/popper.min.js"></script>
+    <script src="<?= base_url(); ?>MDB/js/jquery-3.4.1.min.js"></script>
+</html>
