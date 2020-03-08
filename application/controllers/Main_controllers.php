702080a61b486d89b4c72d8320ffb1b22646e0ac
DMoloko2 <dmoloko2@gmail.com>
2020-03-08 21:00:33 +0300

    логин страница

--- application/controllers/Main_controllers.php
+++ application/controllers/Main_controllers.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main_controllers extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('main_view');
+		$this->load->view('login.php');
 	}
 }
 ?>
